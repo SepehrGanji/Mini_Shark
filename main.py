c5c9c82d283602adb0d5c6d6346b26cf84c704ce
sepehrganji <sepehrganji79@gmail.com>
2020-07-24 22:05:10 +0430

    adding protocol templates and control program flow

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def ether(data):
     dest_mac, src_mac, prot = unpack('! 6s 6s H', data[:14])
     return [get_mac(src_mac), get_mac(dest_mac), htons(prot), data[14:]]
 
+def arp(data):
+    pass
+
 def ip(data):
     mydata = unpack('!BBHHHBBH4s4s', data[:20])
     return [(mydata[0] >> 4), (mydata[0] & 0xF) * 4, mydata[1], mydata[2],
@@ -21,6 +24,22 @@ def ip(data):
     data[((data[0] & 0xF) * 4):]
     ]
 
+def icmp(data):
+    typ, code, checksum = unpack('!BBH', data[:4])
+    return [typ, code, hex(checksum), repr(data[4:])]
+
+def tcp(data):
+    pass
+
+def udp(data):
+    pass
+
+def http(data):
+    pass
+
+def dns(data):
+    pass
+
 connection = socket(AF_PACKET, SOCK_RAW, ntohs(3))
 
 while(True):
@@ -28,17 +47,22 @@ while(True):
     #TODO : save to file
 
     ether_header = ether(data)
-    #TODO : ARP (1544)
+    #TODO : print eth header
 
-    ip_header = ip(ether_header[3])
-    #0 : 4 //IPv4
-    #1 : 20-24 //header length
-    #2 : 0 //type of serviece
-    #3 : //total length
-    #4, 5, 6 : ID, Flag, Offset
-    #7 : TTL
-    #8 : Protocol
-    #9 : Header Checksum
-    #10, 11 : src, dest
-    
-    
+    if(ether_header[2] == 8): #IP
+        ip_header = ip(ether_header[3])#IPv4/header length/type of serviece/total length
+        #ID, Flag, Offset/TTL/Protocol/Header Checksum/src, dest
+        if(ip_header[8] == 1):
+            icmp_header = icmp(ip_header[-1])
+            #TODO : print the rest
+        elif(ip_header[8] == 6):
+            pass
+        elif(ip_header[8] == 17):
+            pass
+        else:
+            pass
+    elif(ether_header[2] == 1544): #ARP
+        #TODO : arp
+        pass
+    else: #Unknown
+        pass
